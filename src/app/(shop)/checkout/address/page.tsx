b5e2054225e32c080862bae4e0f1b0d8ddc00fcf
Efrain Coutiño <l22141208@queretaro.tecnm.mx>
2025-08-12 13:19:20 -0600

    Add admin, cart, category, checkout, empty, order, and product pages

---
+++ src/app/(shop)/checkout/address/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CheckoutAddressPage = () => {
+  return (
+    <div>
+      <h1>Checkout Address Page</h1>
+    </div>
+  );
+};
+
+export default CheckoutAddressPage;
---
+++ src/app/(shop)/orders/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function OrdersListPage() {
+  return (
+    <div>
+      <h1>Orders list Page</h1>
+    </div>
+  );
+}
